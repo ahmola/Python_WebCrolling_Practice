f1e6de2469f608cd23df7fd8f0cc74c48668c156
Jason <66013853+ahmola@users.noreply.github.com>
2020-12-26 00:30:19 +0900

    Change Text File Name

--- Get_HTML/Get_HTML.py
+++ Get_HTML/Get_HTML.py
@@ -16,7 +16,7 @@ print(bsObject)
 f.close()
 
 
-f1 = open('hw5.txt', 'r', encoding='utf-8') #읽기 모드로 다시 열음
+f1 = open('Get_Text.txt', 'r', encoding='utf-8') #읽기 모드로 다시 열음
 f2 = open(t, 'w+', encoding='utf-8') #대문자로 복사할 파일을 선언
 
 text_str = f1.read()
